a39ffb8e4fb71d327d72810e8435f72435e6bbb8
sabertazimi <sabertazimi@gmail.com>
2018-06-01 17:27:36 +0800

    refactor(json): create api for create json data

--- public/_posts/service.js
+++ public/_posts/service.js
@@ -1,88 +1,108 @@
 const path = require('path');
 const fs = require('fs');
-
 const yamlFrontMatter = require('yaml-front-matter');
 
 const rootPath = path.resolve(__dirname);
 const mdPath = path.resolve(rootPath, './md');
 const jsonPath = path.resolve(rootPath, './json');
+const PREVIEW_PER_PAGE = 5; // be same with src/constants/index.js
+const PREVIEW_CHARS = 360;  // be same with src/constants/index.js
 
-const PREVIEW_PER_PAGE = 5;
+class BlogService {
+  constructor(comparator, visitor, accumulator, resolver) {
+    this.comparator = comparator || this.defaultComparator;
+    this.visitor = visitor || this.defaultVisitor;
+    this.accumulator = accumulator || this.defaultAccumulator;
+    this.resolver = resolver || this.defaultResolver;
+    this.init();
+  }
 
-const init = () => {
-  if (!fs.existsSync(jsonPath)) {
-    fs.mkdirSync(jsonPath);
+  init() {
+    if (!fs.existsSync(jsonPath)) {
+      fs.mkdirSync(jsonPath);
+    }
+  }
+
+  /**
+  * defaultComparator
+  *
+  * @param  {string} a file name
+  * @param  {string} b file name
+  * @return {boolean}
+  */
+  defaultComparator(a, b) {
+    return b > a;
+  }
+
+  /**
+  * defaultVisitor
+  *
+  * @param  {Error}  err  markdown file access error
+  * @param  {Buffer} data markdown file data
+  * @return {object}      processed markdown file data
+  */
+  defaultVisitor(err, data) {
+    if (err) {
+      console.error(err);
+      return data;
+    }
+
+    return { data: data.toString().substr(0, 20) };
+  }
+
+  /**
+  * defaultAccumulator
+  *
+  * @param  {object} accum         original accumulated result
+  * @param  {object} processedData current processed markdown file data
+  * @return {object}               added accumulated result
+  */
+  defaultAccumulator(accum, processedData) {
+    const acc = accum.data + processedData.data;
+    return { data: acc };
+  }
+
+  /**
+  * defaultResolver
+  *
+  * @param  {object} accum accumulated result of all visitors
+  * @return {object}       resolved result to return
+  */
+  defaultResolver(accum) {
+    return accum;
+  }
+
+  /**
+  * generateJSON - generate JSON data with several callbacks
+  *
+  * @return {object} final json data
+  */
+  generateJSON() {
+    const files = fs.readdirSync(mdPath);
+    let accum = {};
+
+    if (!files) {
+      return JSONData;
+    }
+
+    files.sort(this.comparator);
+
+    files.forEach((fileName) => {
+      const filePath = path.resolve(mdPath, fileName);
+      const mdData = fs.readFileSync(filePath);
+
+      if (mdData) {
+        const processedData = this.visitor(null, mdData);
+        accum = this.accumulator(accum, processedData);
+      } else {
+        this.visitor(new Error(`[ERR] ${filePath} read failed.`), null);
+      }
+    });
+
+    return this.resolver(accum);
   }
 }
 
-const generatePostsJson = () => {
-  let fileCnt = 0;
-  let mdFilesJson = [];
-  let tagsJson = {};
-
-  fs.readdir(mdPath, (err, files) => {
-    if (err) {
-      console.error(err);
-    } else {
-      files.sort((a, b) => {
-        return b  > a;
-        // return fs.statSync(path.resolve(mdPath, b)).mtime.getTime() -
-        // fs.statSync(path.resolve(mdPath, a)).mtime.getTime();
-      });
-
-      files.forEach((fileName) => {
-        const filePath = path.resolve(mdPath, fileName);
-
-        try {
-          const data = fs.readFileSync(filePath);
-
-          if (data) {
-            let mdFileData = yamlFrontMatter.loadFront(data.toString());
-            mdFileData['fileName'] = fileName;
-
-            // genrate single markdown file json data
-            fs.writeFile(path.resolve(jsonPath, `${fileName}.json`), JSON.stringify(mdFileData), (err) => {
-              if (err) {
-                console.log(err);
-              }
-            });
-
-            // generate markdown tags data
-            if (mdFileData.tags) {
-              mdFileData.tags.forEach((tag) => {
-                if (!tagsJson[tag]) {
-                  tagsJson[tag] = 0;
-                }
-
-                tagsJson[tag] += 1;
-              });
-            }
-
-            // generate markdown files data
-            mdFileData['pageId'] = Math.floor(fileCnt / PREVIEW_PER_PAGE) + 1;
-            mdFilesJson.push(Object.assign({}, mdFileData));
-
-            fileCnt += 1;
-          }
-        } catch (err) {
-          console.error(err);
-        }
-      });
-
-      fs.writeFile(path.resolve(jsonPath, 'tags.json'), JSON.stringify(tagsJson), (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-
-      fs.writeFile(path.resolve(jsonPath, 'posts.json'), JSON.stringify(mdFilesJson), (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-    }
-  });
-}
-
-init();
-generatePostsJson();
+// use
+const getPostsService = new BlogService();
+const JSONData = getPostsService.generateJSON();
