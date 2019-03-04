6e4cfdc46e5b7960db3641a300cfd43f304158ce
sabertazimi <sabertazimi@gmail.com>
2019-03-04 11:22:01 +0800

    refactor(AboutSidebar): change to functional component

--- src/components/AboutSidebar.js
+++ src/components/AboutSidebar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Sidebar,
   Segment
@@ -6,21 +6,17 @@ import {
 
 import GithubCard from './GithubCard.js';
 
-class AboutSidebar extends Component {
-  render() {
-    return (
-      <Sidebar
-        as={Segment}
-        animation='push'
-        width={this.props.width || 'wide'}
-        visible={this.props.visible || false}
-        compact
-        vertical
-        >
-          <GithubCard username='sabertazimi'/>
-      </Sidebar>
-    );
-  }
-}
+const AboutSidebar = ({ width, visible }) => (
+  <Sidebar
+    as={Segment}
+    animation='push'
+    width={width || 'wide'}
+    visible={visible || false}
+    compact
+    vertical
+  >
+    <GithubCard username='sabertazimi'/>
+  </Sidebar>
+);
 
 export default AboutSidebar;
