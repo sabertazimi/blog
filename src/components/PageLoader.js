da5dc4b1fc7f63a479eb9965f02e4fff7a34bebb
sabertazimi <sabertazimi@gmail.com>
2019-03-04 11:59:43 +0800

    refactor(PageLoader): change to functional component

--- src/components/PageLoader.js
+++ src/components/PageLoader.js
@@ -1,19 +1,10 @@
-import React, { Component } from 'react';
-import {
-  Dimmer,
-  Loader,
-} from 'semantic-ui-react';
+import React from 'react';
+import { Dimmer, Loader } from 'semantic-ui-react';
 
-class PageLoader extends Component {
-  render() {
-    const { message } = this.props;
-
-    return (
-      <Dimmer active>
-        <Loader>{ message || 'Loading' }</Loader>
-      </Dimmer>
-    );
-  }
-}
+const PageLoader = ({ message }) => (
+  <Dimmer active>
+    <Loader>{message || 'Loading'}</Loader>
+  </Dimmer>
+);
 
 export default PageLoader;
