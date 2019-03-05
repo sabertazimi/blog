c1baa58b16cc47d4add45ff7e31d1491502bbd00
sabertazimi <sabertazimi@gmail.com>
2019-03-05 21:56:16 +0800

    update(gatsby-graphql): StaticQuery for non-page component
    issue #51

--- gatsby/src/templates/About.jsx
+++ gatsby/src/templates/About.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import { graphql } from "gatsby"
+import { StaticQuery, graphql } from 'gatsby';
 import { SimpleLayout } from '../layouts';
 
-export default ({ data }) => (
-  <SimpleLayout>
-    <div style={{ margin: '3rem auto', maxWidth: 600 }}>
-      <h2>About me</h2>
-      <p style={{ fontSize: '18px' }}>
-        I'm { data.site.siteMetadata.author }, please mail to{' '}
-        <a href="mailto:sabertazimi@gmail.com">sabertazimi@gmail.com</a>
-      </p>
-    </div>
-  </SimpleLayout>
-);
-
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        author
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            author
+          }
+        }
       }
-    }
-  }
-`;
+    `}
+    render={data => (
+      <SimpleLayout>
+        <div style={{ margin: '3rem auto', maxWidth: 600 }}>
+          <h2>About me</h2>
+          <p style={{ fontSize: '18px' }}>
+            I'm {data.site.siteMetadata.author}, please mail to{' '}
+            <a href="mailto:sabertazimi@gmail.com">sabertazimi@gmail.com</a>
+          </p>
+        </div>
+      </SimpleLayout>
+    )}
+  />
+);
