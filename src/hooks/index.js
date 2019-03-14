4b761c001edf1ce966128636b3f23c6e951a24b2
sabertazimi <sabertazimi@gmail.com>
2019-03-15 00:00:04 +0800

    add(hooks-fetch): fetch github data hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,9 +1,11 @@
 import useDisqus from './useDisqus';
 import useForm from './useForm';
 import useResponsive from './useResponsive';
+import useGithub from './useGithub';
 
 export {
   useDisqus,
   useForm,
   useResponsive,
+  useGithub,
 };
