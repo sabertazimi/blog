b47134087eff7d2b4b6b367146b47aedf4baee35
sabertazimi <sabertazimi@gmail.com>
2019-03-28 17:47:49 +0800

    update(ScrollButton): only show one scroll button
    close #63

--- src/components/ScrollButton/ScrollButton.jsx
+++ src/components/ScrollButton/ScrollButton.jsx
@@ -1,24 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useResponsive } from '../../hooks';
-import { Button, Icon } from 'semantic-ui-react';
+import { Button, Icon, Visibility } from 'semantic-ui-react';
 import { BreakPoints, PRIMARY_COLOR } from '../../constants';
 
 const ScrollButton = () => {
+  const [direction, setDirection] = useState('down');
   const isnotMobile = useResponsive({ minWidth: BreakPoints.laptop });
-  const baseStyle = {
+
+  const buttonStyle = {
     position: 'fixed',
     fontSize: isnotMobile ? '16px' : '12px',
     width: isnotMobile ? '100px' : '75px',
     right: isnotMobile ? '50px' : '10px',
-    bottom: isnotMobile ? '15%' : '8%',
+    bottom: isnotMobile ? '5%' : '2%',
   };
-  const topStyle = {
-    ...baseStyle,
-  };
-  const bottomStyle = {
-    ...baseStyle,
-    bottom: isnotMobile ? '7%' : '2%',
-  };
+
+  const onScroll = (e, { calculations }) =>
+    setDirection(calculations.direction);
 
   const scrollTo = (event, data) => {
     event.preventDefault();
@@ -50,34 +48,41 @@ const ScrollButton = () => {
   }, []);
 
   return (
-    <div>
-      <Button
-        style={topStyle}
-        animated="fade"
-        color={PRIMARY_COLOR}
-        size="large"
-        direction="up"
-        onClick={scrollTo}
-      >
-        <Button.Content visible>Top</Button.Content>
-        <Button.Content hidden>
-          <Icon name="up arrow" />
-        </Button.Content>
-      </Button>
-      <Button
-        style={bottomStyle}
-        animated="fade"
-        color={PRIMARY_COLOR}
-        size="large"
-        direction="down"
-        onClick={scrollTo}
-      >
-        <Button.Content visible>Bottom</Button.Content>
-        <Button.Content hidden>
-          <Icon name="down arrow" />
-        </Button.Content>
-      </Button>
-    </div>
+    <Visibility onUpdate={onScroll} once={false}>
+      {direction === 'up' ? (
+        <Button
+          style={buttonStyle}
+          animated="fade"
+          color={PRIMARY_COLOR}
+          size="large"
+          direction="up"
+          onClick={scrollTo}
+        >
+          <Button.Content visible>
+            <Icon name="anchor" />
+          </Button.Content>
+          <Button.Content hidden>
+            <Icon name="up arrow" />
+          </Button.Content>
+        </Button>
+      ) : (
+        <Button
+          style={buttonStyle}
+          animated="fade"
+          color={PRIMARY_COLOR}
+          size="large"
+          direction="down"
+          onClick={scrollTo}
+        >
+          <Button.Content visible>
+            <Icon name="anchor" />
+          </Button.Content>
+          <Button.Content hidden>
+            <Icon name="down arrow" />
+          </Button.Content>
+        </Button>
+      )}
+    </Visibility>
   );
 };
 
