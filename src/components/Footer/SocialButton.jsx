cdb76f178daa9f3aae2c3b6ba0e5cc96ecf82c41
sabertazimi <sabertazimi@gmail.com>
2019-03-28 10:20:37 +0800

    update(Footer): tidy code up

--- src/components/Footer/SocialButton.jsx
+++ src/components/Footer/SocialButton.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import { Icon, Button } from 'semantic-ui-react';
 
 const SocialButton = ({ type, url }) => (
-  <Button as="a" icon circular href={`${url}`} color="black" style={{ margin: '0 1em' }}>
+  <Button
+    as="a"
+    icon
+    circular
+    href={`${url}`}
+    color="black"
+    style={{ margin: '0 1em' }}
+  >
     <Icon name={`${type}`} size="big" />
   </Button>
 );
