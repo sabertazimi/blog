ef59d7166738ebee208462da8921b13615d364d1
sabertazimi <sabertazimi@gmail.com>
2019-03-28 10:43:28 +0800

    update(metadata): add website metadata constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,11 @@
-export const PREVIEW_NUM = 5;
-export const PREVIEW_CHARS = 200;
 export const PRIMARY_COLOR = 'blue';
-export const PREVIEW_PER_PAGE = 5;
+
 export const BreakPoints = {
   laptop: 960,
   desktop: 1280,
+};
+
+export const MetaData = {
+  title: 'Sabertazimi \' Blog',
+  url: 'https://tazimi.dev',
 };
