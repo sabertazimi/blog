53624afc36dc89a6169a99b7f79dbf2ae349732b
sabertazimi <sabertazimi@gmail.com>
2019-03-30 01:40:46 +0800

    update(IconBanner-antd): switch to antd
    issue #67

--- src/components/IconBanner/IconBanner.jsx
+++ src/components/IconBanner/IconBanner.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { Image, Icon, Popup } from 'semantic-ui-react';
+import { Tooltip, Icon } from 'antd';
+import { Image, Popup } from 'semantic-ui-react';
 import styles from './IconBanner.module.css';
 import banner from '../../images/banner.png';
 
@@ -8,46 +9,26 @@ const IconBanner = () => (
   <React.Fragment>
     <Image src={banner} size="small" alt="banner" />
     <div className={styles.iconBanner}>
-      <Popup
-        position="left center"
-        inverted
-        content="Posts"
-        trigger={
-          <Link to="/posts">
-            <Icon name="list ul" size="big" className={styles.iconBannerLink} />
-          </Link>
-        }
-      />
-      <Popup
-        position="left center"
-        inverted
-        content="Tags"
-        trigger={
+      <Tooltip placement="left" title="View Posts">
+        <Link to="/posts">
+          <Icon type="bars" className={styles.iconBannerLink} />
+        </Link>
+      </Tooltip>
+      <Tooltip placement="left" title="View Tags">
           <Link to="/tags">
-            <Icon name="tags" size="big" className={styles.iconBannerLink} />
+            <Icon type="tags" className={styles.iconBannerLink} />
           </Link>
-        }
-      />
-      <Popup
-        position="left center"
-        inverted
-        content="Books"
-        trigger={
+      </Tooltip>
+      <Tooltip placement="left" title="View Books">
           <Link to="/books">
-            <Icon name="book" size="big" className={styles.iconBannerLink} />
+            <Icon type="book" className={styles.iconBannerLink} />
           </Link>
-        }
-      />
-      <Popup
-        position="left center"
-        inverted
-        content="About"
-        trigger={
+      </Tooltip>
+      <Tooltip placement="left" title="About Me">
           <Link to="/about">
-            <Icon name="user" size="big" className={styles.iconBannerLink} />
+            <Icon type="user" className={styles.iconBannerLink} />
           </Link>
-        }
-      />
+      </Tooltip>
     </div>
   </React.Fragment>
 );
