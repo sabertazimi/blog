9a0185a8f221ceae1636c0eea47a775040f1b059
sabertazimi <sabertazimi@gmail.com>
2021-07-14 22:05:07 +0800

    add(404): create 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Layout } from 'layouts';
+
+const NotFoundPage = () => {
+  return (
+    <Layout banner="Exploring">
+      <div className="text-center">
+        <h1>Sorry, the page you visited does not exist.</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
