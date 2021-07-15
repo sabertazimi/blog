a509d5969ee9a1bb37982818eb734285e18acbd2
sabertazimi <sabertazimi@gmail.com>
2021-07-15 15:28:31 +0800

    refactor(routes): seperate routes config

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 import { BreakPoints } from './responsive';
 import { Colors, getRandomColor } from './colors';
 import { SocialType, SocialQuery } from './social';
+import Routes from './routes';
 
-export { BreakPoints, Colors, getRandomColor, SocialType, SocialQuery };
+export { BreakPoints, Colors, getRandomColor, SocialType, SocialQuery, Routes };
---
+++ src/config/routes.js
@@ -0,0 +1,24 @@
+const Routes = [
+  {
+    id: 'posts',
+    name: 'Posts',
+    path: '/posts',
+  },
+  {
+    id: 'tags',
+    name: 'Tags',
+    path: '/tags',
+  },
+  {
+    id: 'books',
+    name: 'Books',
+    path: '/books',
+  },
+  {
+    id: 'about',
+    name: 'About',
+    path: '/about',
+  },
+];
+
+export default Routes;
