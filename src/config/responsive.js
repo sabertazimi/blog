f0d4e2a6bf6efa41c0867a611e21115561480564
sabertazimi <sabertazimi@gmail.com>
2021-07-15 15:40:58 +0800

    update(responsive): add smallest break point

--- src/config/responsive.js
+++ src/config/responsive.js
@@ -1,10 +1,14 @@
 const BreakPoints = {
-  mobile: 768,
+  mobile: 640,
+  tablet: 768,
   laptop: 1024,
   desktop: 1280,
   tv: 1536,
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  '2xl': 1536,
 };
 
-export {
-  BreakPoints,
-};
+export { BreakPoints };
