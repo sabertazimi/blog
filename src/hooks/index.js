c0479ad61b9054fdbf65cb40fe50f7d948a5fd7d
sabertazimi <sabertazimi@gmail.com>
2021-07-20 19:09:46 +0800

    update(hooks): remove useResponsive hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,12 +1,10 @@
 import useDisqus from './useDisqus';
-import useResponsive from './useResponsive';
 import useBuildTime from './useBuildTime';
 import useSiteMetadata from './useSiteMetadata';
 import usePostsMetadata from './usePostsMetadata';
 
 export {
   useDisqus,
-  useResponsive,
   useBuildTime,
   useSiteMetadata,
   usePostsMetadata,
