3cdac0132d25d89a2450ea4b1e9c577551665462
sabertazimi <sabertazimi@gmail.com>
2021-07-21 02:50:32 +0800

    refactor(className): warp className with classnames util
    issue #83

--- src/components/LandingNav/LandingNavLink.jsx
+++ src/components/LandingNav/LandingNavLink.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import classNames from 'classnames';
 import { Link } from 'gatsby';
 import { Tooltip } from 'antd';
 
 const LandingNavLink = ({ title = 'Tooltip', to = '/', children }) => (
   <Tooltip placement="left" title={title} className="mb-16">
     <Link to={to}>
-      <span className="block px-0 pt-0 pb-3 mx-6 my-0 text-4xl font-extrabold leading-none transition-all scale-100 border-solid cursor-pointer border-b-6 border-light text-light md:text-7xl opacity-80 hover:opacity-100 hover:scale-110">
+      <span
+        className={classNames(
+          'block px-0 pt-0 pb-3 mx-6 my-0 border-solid border-b-6 border-light',
+          'text-4xl font-extrabold leading-none cursor-pointer text-light md:text-7xl',
+          'transition-all scale-100 opacity-80 hover:opacity-100 hover:scale-110'
+        )}
+      >
         {children}
       </span>
     </Link>
