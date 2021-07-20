c46703a4f35e9657b7417db7bc94fec37a1e2c69
sabertazimi <sabertazimi@gmail.com>
2021-07-20 19:07:10 +0800

    update(responsive): remove JavaScript break points
    issue #77

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,5 @@
-import { BreakPoints } from './responsive';
 import { Colors, getRandomColor } from './colors';
 import { SocialType, SocialQuery } from './social';
 import Routes from './routes';
 
-export { BreakPoints, Colors, getRandomColor, SocialType, SocialQuery, Routes };
+export { Colors, getRandomColor, SocialType, SocialQuery, Routes };
