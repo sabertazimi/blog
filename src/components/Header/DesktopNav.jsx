727d602072685bc920636d73a4bf4f514d7cd0de
sabertazimi <sabertazimi@gmail.com>
2021-07-21 23:51:27 +0800

    refactor(DesktopNav): polish className up
    issue #83

--- src/components/Header/DesktopNav.jsx
+++ src/components/Header/DesktopNav.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import { Link } from 'gatsby';
 import { Menu } from 'antd';
 import { Routes } from '@config';
@@ -7,20 +8,18 @@ import logo from 'images/logo-full.png';
 
 const DesktopNav = ({ fixed }) => (
   <Menu
-    className={
-      fixed
-        ? 'fixed z-100 w-full font-extrabold transition transform-gpu bg-white border-transparent hidden md:visible md:flex'
-        : 'fixed z-100 w-full font-extrabold transition transform-gpu bg-gradient-primary border-transparent hidden md:visible md:flex'
-    }
+    className={classNames(
+      'fixed z-100 w-full font-extrabold transition transform-gpu border-transparent hidden md:visible md:flex',
+      { 'bg-white': fixed, 'bg-gradient-primary': !fixed }
+    )}
     mode="horizontal"
   >
     <Menu.Item
       key="home"
-      className={
-        fixed
-          ? 'flex-container transition scale-0 transform-gpu'
-          : 'flex-container transition scale-100 transform-gpu '
-      }
+      className={classNames('flex-container transition transform-gpu', {
+        'scale-0': fixed,
+        'scale-100': !fixed,
+      })}
     >
       <Link to="/">
         <img className="block w-full h-24" src={logo} alt="Logo" />
@@ -29,16 +28,16 @@ const DesktopNav = ({ fixed }) => (
     {Routes.map((route) => (
       <Menu.Item
         key={route.id}
-        className={
-          fixed
-            ? 'flex-container transition -translate-x-28 transform-gpu'
-            : 'flex-container transition translate-x-0 transform-gpu '
-        }
+        className={classNames('flex-container transition transform-gpu', {
+          '-translate-x-28': fixed,
+          'translate-x-0': !fixed,
+        })}
       >
         <Link
-          className={
-            fixed ? 'transition-none text-dark' : 'transition-none text-light'
-          }
+          className={classNames('transition-none', {
+            'text-dark': fixed,
+            'text-light': !fixed,
+          })}
           to={route.path}
         >
           {route.name}
@@ -47,11 +46,13 @@ const DesktopNav = ({ fixed }) => (
     ))}
     <Menu.Item
       key="search"
-      className={
-        fixed
-          ? 'flex-container border-transparent transition -translate-x-28 transform-gpu'
-          : 'flex-container border-transparent transition translate-x-0 transform-gpu'
-      }
+      className={classNames(
+        'flex-container border-transparent transition transform-gpu',
+        {
+          '-translate-x-28': fixed,
+          'translate-x-0': !fixed,
+        }
+      )}
     >
       <PostsSearchBar />
     </Menu.Item>
