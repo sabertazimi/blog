895a7abd2f57f6a40189f90625607a961dc6a551
sabertazimi <sabertazimi@gmail.com>
2021-08-04 16:51:21 +0800

    fix(ArticleFooter): production environment Ads free
    close #108

--- src/components/Article/ArticleComments.tsx
+++ src/components/Article/ArticleComments.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { useDisqus, useSiteMetadata } from '@hooks';
 import * as styles from './ArticleComments.module.css';
 
-/**
- * @TODO Looking for Disqus alternative due to Ads
- */
 const ArticleComments = () => {
   const { disqusUrl } = useSiteMetadata();
   useDisqus(disqusUrl);
