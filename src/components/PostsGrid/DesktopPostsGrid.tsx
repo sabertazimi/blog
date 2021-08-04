b44f900c3037465155689032ab1f7c4d702984f1
sabertazimi <sabertazimi@gmail.com>
2021-08-04 16:48:15 +0800

    fix(DesktopPostsGrid): add missing types
    close #124
    issue #82

--- src/components/PostsGrid/DesktopPostsGrid.tsx
+++ src/components/PostsGrid/DesktopPostsGrid.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import FlexContainer from '@components/FlexContainer';
 import PostCard from './PostCard';
+import { PostType } from '@types';
 
-// @FIXME: Bug: missing TypeScript types
-const DesktopPostsGrid = ({ posts }) => {
+interface Props {
+  posts: PostType[];
+}
+
+const DesktopPostsGrid = ({ posts }: Props): JSX.Element => {
   const leftPosts = posts.slice(0, Math.ceil(posts.length / 2));
   const rightPosts = posts.slice(Math.ceil(posts.length / 2));
 
