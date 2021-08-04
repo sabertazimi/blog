0acf22d16dffd958fd9c236e26c41d5d5a7d0a7a
sabertazimi <sabertazimi@gmail.com>
2021-08-04 22:36:57 +0800

    test(SocialButton): complete GitHub icon testing
    close #64

--- src/components/SocialButton/SocialButton.test.tsx
+++ src/components/SocialButton/SocialButton.test.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
+import { render } from '@testing-library/react';
 import SocialButton from './SocialButton';
 
-describe('Header', () => {
-  it('renders correctly', () => {
+describe('SocialButton', () => {
+  test('render GitHub correctly (snapshot)', () => {
     const tree = renderer
       .create(<SocialButton type="github" url="https://github.com/" />)
       .toJSON();
+
     expect(tree).toMatchSnapshot();
+  });
+
+  test('render GitHub with correct structure', () => {
+    const { getByRole } = render(
+      <SocialButton type="github" url="https://github.com/" />
+    );
+    const link = getByRole('link');
+    const icon = getByRole('img');
+
+    expect(link).toBeInTheDocument();
+    expect(icon).toBeInTheDocument();
+    expect(link).toContainElement(icon);
+  });
+
+  test('render GitHub with correct structure', () => {
+    const { getByRole } = render(
+      <SocialButton type="github" url="https://github.com/" />
+    );
+    const link = getByRole('link');
+
+    expect(link).toHaveAttribute('href', 'https://github.com/');
   });
 });
