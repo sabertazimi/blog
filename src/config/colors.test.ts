0567e798c3691099faa05943a1b8f57bea0d33f2
sabertazimi <sabertazimi@gmail.com>
2021-08-05 00:17:51 +0800

    test(colors): complete `colors.getColorByName` testing

---
+++ src/config/colors.test.ts
@@ -0,0 +1,28 @@
+import { ColorPalette, getColorByName } from './colors';
+
+describe('Colors', () => {
+  const tagsColor = [
+    ['CS', ColorPalette.grape],
+    ['CSS', ColorPalette.violet],
+    ['Git', ColorPalette.violet],
+    ['Gnu', ColorPalette.violet],
+    ['Vim', ColorPalette.violet],
+    ['HTML', ColorPalette.indigo],
+    ['Linux', ColorPalette.blue],
+    ['React', ColorPalette.blue],
+    ['Redux', ColorPalette.blue],
+    ['Security', ColorPalette.green],
+    ['JavaScript', ColorPalette.yellow],
+    ['Development', ColorPalette.orange],
+    ['Architecture', ColorPalette.red],
+    ['React Router', ColorPalette.red],
+    ['Design Patterns', ColorPalette.violet],
+    ['Web Development', ColorPalette.violet],
+    ['Computer Science', ColorPalette.indigo],
+    ['Frontend Development', ColorPalette.green],
+  ];
+
+  test.each(tagsColor)('should set color of tag [%s] to [%s]', (tag, color) => {
+    expect(getColorByName(tag)).toBe(color);
+  });
+});
