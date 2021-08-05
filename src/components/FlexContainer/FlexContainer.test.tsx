7d03d9ff419485a5d9d542fb4e3e96225a559961
sabertazimi <sabertazimi@gmail.com>
2021-08-06 01:19:09 +0800

    test(FlexContainer): add snapshot testing

---
+++ src/components/FlexContainer/FlexContainer.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import FlexContainer from './FlexContainer';
+
+describe('Container', () => {
+  test('should render correctly (snapshot)', () => {
+    const tree = renderer
+      .create(
+        <FlexContainer role="main">
+          <h1>FlexContainer</h1>
+        </FlexContainer>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('should render children correctly', () => {
+    const { getByRole } = render(
+      <FlexContainer role="main">
+        <h1>FlexContainer</h1>
+      </FlexContainer>
+    );
+    const container = getByRole('main');
+
+    expect(container.firstChild).toHaveTextContent('FlexContainer');
+  });
+});
