68f8fd7586d066f14409e63035579d391c29b92d
sabertazimi <sabertazimi@gmail.com>
2021-08-06 22:16:46 +0800

    test(Header): add snapshot testing

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+import Header from './Header';
+
+const basePosts = Array.from(Array(5).keys()).map((post) => ({
+  slug: `/${post}BasicNotes/`,
+  timeToRead: post,
+}));
+const posts = basePosts.map((post, index) => ({
+  ...post,
+  title: `${index} Basic Notes`,
+  subtitle: 'Be a Stupid Learner',
+  author: 'Sabertaz',
+  date: '2018-08-08T00:00:00.000Z',
+  tags: ['JavaScript', 'Frontend Development', 'Web Development'],
+  prevPost: {
+    slug: `/${index}BasicNotes/`,
+    title: `${index} Basic Notes`,
+  },
+  nextPost: {
+    slug: `/${index} AdvancedNotes/`,
+    title: `${index} Advanced Notes`,
+  },
+}));
+
+describe('Header', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  test('should render correctly (snapshot)', () => {
+    const tree = create(<Header posts={posts} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
