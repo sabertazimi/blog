754029c9a49840d49a2a23105f248367ba25d0a0
sabertazimi <sabertazimi@gmail.com>
2021-08-08 09:25:25 +0800

    test(TypingTitle): setup mock for typed.js module

--- src/components/TypingTitle/TypingTitle.test.tsx
+++ src/components/TypingTitle/TypingTitle.test.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { create } from 'react-test-renderer';
 import TypingTitle from './TypingTitle';
 
+jest.mock('typed.js');
+
 const landingTitles = [`I'm a coder.`, `I'm a learner.`];
 
 describe('TypingTitle', () => {
   test('should render correctly (snapshot)', () => {
-    const tree = create(<TypingTitle titles={landingTitles} />).toJSON();
+    const renderer = create(<TypingTitle titles={landingTitles} />);
+    const tree = renderer.toJSON();
     expect(tree).toMatchSnapshot();
+    renderer.unmount();
   });
 });
