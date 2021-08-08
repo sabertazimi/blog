a073c6f098ec27d7fb1642ed260e1e6c7c000faa
sabertazimi <sabertazimi@gmail.com>
2021-08-08 16:32:11 +0800

    test(useVisibility): imporve code coverage

--- src/hooks/useVisibility.test.tsx
+++ src/hooks/useVisibility.test.tsx
@@ -1,11 +1,24 @@
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import React, { useRef } from 'react';
 import useVisibility from './useVisibility';
 
+const onBottomPassed = jest.fn();
+const onBottomPassedReverse = jest.fn();
+
 const Header = (): JSX.Element => {
   const headerRef = useRef<HTMLDivElement>(null);
-  const onBottomPassed = jest.fn();
-  const onBottomPassedReverse = jest.fn();
+
+  useVisibility({
+    ref: headerRef,
+    onBottomPassed,
+    onBottomPassedReverse,
+  });
+
+  return <div>Header</div>;
+};
+
+const HeaderWithRef = (): JSX.Element => {
+  const headerRef = useRef<HTMLDivElement>(null);
 
   useVisibility({
     ref: headerRef,
@@ -14,21 +27,73 @@ const Header = (): JSX.Element => {
   });
 
   return (
-    <div
-      ref={headerRef}
-      style={{ display: 'block', width: '100%', height: '100vh' }}
-    >
-      App
+    <div>
+      <div
+        ref={headerRef}
+        style={{
+          boxSizing: 'border-box',
+          display: 'block',
+          margin: '0 auto',
+          padding: '0',
+          width: '100%',
+          height: '50px',
+        }}
+      >
+        Header
+      </div>
+      <div
+        style={{
+          boxSizing: 'border-box',
+          display: 'block',
+          margin: '0 auto',
+          padding: '0',
+          width: '100%',
+          height: '150vh',
+        }}
+      >
+        Body
+      </div>
+      <div
+        style={{
+          boxSizing: 'border-box',
+          display: 'block',
+          margin: '0 auto',
+          padding: '0',
+          width: '100%',
+          height: '50px',
+        }}
+      ></div>
     </div>
   );
 };
 
 describe('useVisibility', () => {
-  test('should work correctly when scrolling and resizing', () => {
-    const { container, rerender } = render(<Header />);
-    fireEvent.scroll(window, { screenY: 50 });
-    rerender(<Header />);
-    fireEvent.resize(container, { innerWidth: 100, innerHeight: 100 });
-    rerender(<Header />);
+  let mockConsoleError: jest.SpyInstance;
+
+  beforeEach(() => {
+    mockConsoleError = jest.spyOn(console, 'error').mockImplementation(() => {
+      return;
+    });
+  });
+
+  afterEach(() => {
+    mockConsoleError.mockRestore();
+  });
+
+  test('should log error message when missing ref', async () => {
+    render(<Header />);
+    fireEvent.scroll(window, { target: { scrollY: 100 } });
+    await waitFor(() => {
+      expect(mockConsoleError).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  test('should invoke callbacks when scrolling', async () => {
+    render(<HeaderWithRef />);
+    fireEvent.scroll(window, { target: { scrollY: 0 } });
+    await waitFor(() => {
+      expect(onBottomPassed).not.toBeCalled();
+      expect(onBottomPassedReverse).not.toBeCalled();
+    });
   });
 });
