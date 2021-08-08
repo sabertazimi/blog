15cc7610d42bb2afb655f8d91b7d4549fa0a2dbf
sabertazimi <sabertazimi@gmail.com>
2021-08-08 11:12:07 +0800

    refactor(ArticleComments): format testing code

--- src/components/Article/ArticleComments.test.tsx
+++ src/components/Article/ArticleComments.test.tsx
@@ -5,16 +5,6 @@ import ArticleComments from './ArticleComments';
 const commentUrl = 'https://example.com';
 
 describe('ArticleComments', () => {
-  beforeAll(() => {
-    jest.spyOn(document, 'getElementById').mockImplementation((elementId) => {
-      return document.createElement(`#${elementId}`);
-    });
-  });
-
-  afterAll(() => {
-    (document.getElementById as unknown as jest.SpyInstance).mockRestore();
-  });
-
   test('should render correctly (snapshot)', () => {
     const tree = create(<ArticleComments url={commentUrl} />).toJSON();
     expect(tree).toMatchSnapshot();
