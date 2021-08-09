568602b9748a16ea2401193b68e39b6b95bf5092
sabertazimi <sabertazimi@gmail.com>
2021-08-10 02:59:52 +0800

    fix(pages-404): rectify css modules order conflict

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,20 @@
+import { MetaHeader } from '@components';
+import { useSiteMetadata } from '@hooks';
 import { Layout } from '@layouts';
 import React from 'react';
 
 const NotFoundPage = (): JSX.Element => {
+  const { siteUrl, title } = useSiteMetadata();
+
   return (
-    <Layout banner="Exploring">
-      <div className="text-center">
-        <h1>Sorry, the page you visited does not exist.</h1>
-      </div>
-    </Layout>
+    <div>
+      <MetaHeader siteUrl={siteUrl} title={title} />
+      <Layout banner="Exploring">
+        <div className="text-center">
+          <h1>Sorry, the page you visited does not exist.</h1>
+        </div>
+      </Layout>
+    </div>
   );
 };
 
