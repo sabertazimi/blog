e14432a8a289d1abc2ea118832524658692add45
Sabertaz <sabertazimi@gmail.com>
2022-02-13 10:57:57 +0800

    fix(jest): rectify jest module mapper (#508)

--- jest.config.js
+++ jest.config.js
@@ -31,6 +31,7 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/jest.mock.js',
+     '^gatsby-page-utils/(.*)$': 'gatsby-page-utils/dist/$1', // Workaround for https://github.com/facebook/jest/issues/9771.
     ...paths,
   },
   testPathIgnorePatterns: [
