77688a596d6b6a194243b198ab6ad7fdfa31965b
sabertazimi <sabertazimi@gmail.com>
2022-04-02 17:04:20 +0800

    fix(LandingNavLink): drop antd `Tooltip` component
    Due to antd's lack of supporting for
    [React v18](https://github.com/ant-design/ant-design/labels/React%2018),
    remove usage of antd `Tooltip`.

--- src/components/LandingNav/LandingNavLink.tsx
+++ src/components/LandingNav/LandingNavLink.tsx
@@ -1,4 +1,3 @@
-import { Tooltip } from 'antd';
 import classNames from 'classnames';
 import { Link } from 'gatsby';
 import type { ReactNode } from 'react';
@@ -10,20 +9,18 @@ interface Props {
   children: ReactNode;
 }
 
-const LandingNavLink = ({ title, to, children }: Props): JSX.Element => (
-  <Tooltip className="mb-16" placement="left" title={title}>
-    <Link to={to} role="link">
-      <span
-        className={classNames(
-          'block px-0 pt-0 pb-3 mx-6 my-0 border-solid border-b-6 border-light',
-          'text-4xl font-extrabold leading-none cursor-pointer text-light md:text-7xl',
-          'transition-all transform-gpu scale-100 opacity-80 hover:opacity-100 hover:scale-110'
-        )}
-      >
-        {children}
-      </span>
-    </Link>
-  </Tooltip>
+const LandingNavLink = ({ to, children }: Props): JSX.Element => (
+  <Link to={to} role="link">
+    <span
+      className={classNames(
+        'block px-0 pt-0 pb-3 mx-6 mb-16 border-solid border-b-6 border-light',
+        'text-4xl font-extrabold leading-none cursor-pointer text-light md:text-7xl',
+        'transition-all transform-gpu scale-100 opacity-80 hover:opacity-100 hover:scale-110'
+      )}
+    >
+      {children}
+    </span>
+  </Link>
 );
 
 export default LandingNavLink;
