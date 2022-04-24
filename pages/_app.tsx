225753ca0d99fe0892b9afc04a90da985c178ed7
Sabertaz <sabertazimi@gmail.com>
2022-04-24 22:36:18 +0800

    feature(routing progress) add page routing progress bar (#635)
    close #627

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
+import { ColorPalette } from '@config';
+import NextProgress from 'next-progress';
+import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import 'antd/dist/antd.css';
 import '../styles/tailwind.css';
-import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component className="min-h-screen" {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <NextProgress height={3} color={ColorPalette['blue']} />
+      <Component className="min-h-screen" {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
