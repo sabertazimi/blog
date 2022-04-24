fb9985776ebfa0dc8e338afedb67e0a2c4dc877c
sabertazimi <sabertazimi@gmail.com>
2022-04-24 16:45:59 +0800

    fix(antd-react 18): compatibility for React 18
    revert #576
    close #587

--- components/ErrorBoundary/ErrorBoundary.tsx
+++ components/ErrorBoundary/ErrorBoundary.tsx
@@ -34,8 +34,6 @@ class ErrorBoundary extends Component<Props, State> {
 
     if (hasError && isDevelopment) {
       return (
-        // @TODO: Remove comment until ant-design/ant-design#34945 merged.
-        // @ts-expect-error Wait for https://github.com/ant-design/ant-design/pull/34945.
         <Result status="error" title="Some Error Happened">
           <div role="alert">
             <Typography.Paragraph>
--- components/LandingNav/LandingNavLink.tsx
+++ components/LandingNav/LandingNavLink.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from 'antd';
 import classNames from 'classnames';
 import Link from 'next/link';
 import type { ReactNode } from 'react';
@@ -5,22 +6,24 @@ import React from 'react';
 
 interface Props {
   title: string;
-  to: string;
+  href: string;
   children: ReactNode;
 }
 
-const LandingNavLink = ({ to, children }: Props): JSX.Element => (
-  <Link href={to}>
-    <a
+const LandingNavLink = ({ title, href, children }: Props): JSX.Element => (
+  <Tooltip className="mb-16" placement="left" title={title}>
+    <span
       className={classNames(
-        'block px-0 pt-0 pb-3 mx-6 mb-16 border-solid border-b-6 border-light',
+        'block px-0 pt-0 pb-3 mx-6 my-0 border-solid border-b-6 border-light',
         'text-4xl font-extrabold leading-none cursor-pointer text-light md:text-7xl',
         'transition-all transform-gpu scale-100 opacity-80 hover:opacity-100 hover:scale-110'
       )}
     >
-      {children}
-    </a>
-  </Link>
+      <Link href={href}>
+        <a>{children}</a>
+      </Link>
+    </span>
+  </Tooltip>
 );
 
 export default LandingNavLink;
