69d1fbfc0cb9800e738a342d84f36dc4771168e0
sabertazimi <sabertazimi@gmail.com>
2022-04-24 23:45:28 +0800

    fix(DesktopNav-Menu): drop legacy `<Menu.item>` compound component

--- components/Header/DesktopNav.tsx
+++ components/Header/DesktopNav.tsx
@@ -17,59 +17,58 @@ const DesktopNav = ({ fixed, posts }: Props): JSX.Element => (
   <Menu
     mode="horizontal"
     className={classNames(
-      'fixed top-0 z-100 w-full font-extrabold bg-transparent border-transparent',
-      'transition transform-gpu hidden md:visible md:flex',
+      'fixed top-0 z-100 w-full',
+      'hidden md:visible md:flex',
+      'font-extrabold bg-transparent border-transparent',
+      'transition transform-gpu',
       {
+        'backdrop-blur-none': !fixed,
         'bg-gradient-primary': !fixed,
+        'shadow-none': !fixed,
+        'backdrop-blur-sm': fixed,
         'bg-white/30': fixed,
         'shadow-lg': fixed,
-        'backdrop-blur-primary': fixed,
       }
     )}
-  >
-    <Link href="/" key="home">
-      <a
-        className={classNames('flex-container ml-auto mr-6 transition transform-gpu', {
-          'scale-100': !fixed,
-          'scale-0': fixed,
-        })}
-      >
-        <Image src={logo} alt="Logo" width={96} height={96} />
-      </a>
-    </Link>
-    {Routes.map(route => (
-      <Menu.Item
-        key={route.id}
-        className={classNames('flex-container transition transform-gpu', {
-          'translate-x-0': !fixed,
-          '-translate-x-28': fixed,
-        })}
-      >
-        <Link href={route.path}>
-          <a
-            className={classNames('transition-none', {
-              'text-light': !fixed,
-              'text-dark': fixed,
-            })}
-          >
-            {route.name}
-          </a>
-        </Link>
-      </Menu.Item>
-    ))}
-    <Menu.Item
-      key="search"
-      className={classNames(
-        'flex-container ml-auto mr-64 border-transparent transition transform-gpu',
-        {
-          'translate-x-0': !fixed,
-          '-translate-x-28': fixed,
-        }
-      )}
-    >
-      <PostsSearchBar posts={posts} />
-    </Menu.Item>
-  </Menu>
+    items={[
+      {
+        key: 'home',
+        label: (
+          <Link href="/">
+            <a className="flex-container transition transform-gpu">
+              <Image src={logo} alt="Logo" width={96} height={96} />
+            </a>
+          </Link>
+        ),
+      },
+      ...Routes.map(route => ({
+        key: route.id,
+        label: (
+          <Link href={route.path}>
+            <a
+              className={classNames(
+                'flex-container h-full transition transform-gpu transition-none',
+                {
+                  'text-light': !fixed,
+                  'text-dark': fixed,
+                }
+              )}
+            >
+              {route.name}
+            </a>
+          </Link>
+        ),
+      })),
+      {
+        key: 'search',
+        label: (
+          <span className="flex-container h-full border-transparent transition transform-gpu">
+            <PostsSearchBar posts={posts} />
+          </span>
+        ),
+      },
+    ]}
+  />
 );
 
 export default DesktopNav;
