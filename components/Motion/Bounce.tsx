ef2d5df2cf4aca730c275478a3090ddeb8e882f9
Sabertaz <sabertazimi@gmail.com>
2022-04-25 04:41:14 +0800

    fix(motion-bounce): add `className` prop to `Bounce` (#642)
    issue #619

--- components/Motion/Bounce.tsx
+++ components/Motion/Bounce.tsx
@@ -8,11 +8,17 @@ interface Props {
 const Bounce = ({ children, ...props }: Props): JSX.Element => (
   <motion.div
     className="inline-flex"
-    initial={{ opacity: 0, translateX: 20 }}
-    whileInView={{ opacity: 1, translateX: 0 }}
+    layout
+    initial={{ opacity: 0, x: 20 }}
+    whileInView={{ opacity: 1, x: 0 }}
+    whileHover={{ scale: 1.2, y: -5 }}
+    whileTap={{ scale: 1.0, y: 0 }}
+    transition={{
+      type: 'spring',
+      stiffness: 100,
+      duration: 0.2,
+    }}
     viewport={{ once: true }}
-    whileHover={{ scale: 1.2, translateY: -5 }}
-    whileTap={{ scale: 1.0, translateY: 0 }}
     {...props}
   >
     {children}
