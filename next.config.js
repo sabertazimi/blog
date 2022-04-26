e7cccddb5b22da7dad1afda418b772264a800cae
Sabertaz <sabertazimi@gmail.com>
2022-04-26 11:43:13 +0800

    fix(a11y): add `lang` to `<html>` tag (#664)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   eslint: {
     dirs: ['components', 'config', 'hooks', 'layouts', 'lib', 'pages', 'types'],
   },
+  i18n: {
+    locales: ['en-US', 'zh-CN'],
+    defaultLocale: 'zh-CN',
+  },
   reactStrictMode: true,
   swcMinify: true,
 };
