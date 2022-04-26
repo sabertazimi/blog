2e4a8c7dda20a2d88c69b5987a9fabecefb82fbd
Sabertaz <sabertazimi@gmail.com>
2022-04-26 10:22:44 +0800

    chore(next): format configuration file (#663)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   eslint: {
     dirs: ['components', 'config', 'hooks', 'layouts', 'lib', 'pages', 'types'],
   },
+  reactStrictMode: true,
   swcMinify: true,
 };
 
