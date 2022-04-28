53a52b2005b87a786ad4036cd496b8d2c0576db6
Sabertaz <sabertazimi@gmail.com>
2022-04-29 06:16:49 +0800

    fix(styles): keep `antd` styles take precedence over `tailwind` styles. (#715)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import { siteConfig } from '@config';
-import 'antd/dist/antd.css';
 import NextProgress from 'next-progress';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import '../styles/tailwind.css';
+import 'antd/dist/antd.css';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
   <>
