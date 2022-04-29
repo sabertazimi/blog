7f8fea5af1655bcde2124f71244403296211bf95
Sabertaz <sabertazimi@gmail.com>
2022-04-30 01:44:05 +0800

    feat(mdx-code): add MacOS style flag and language detection support (#730)
    * feat(mdx-code): add MacOS style flag and language detection support
    
    issue #699
    issue #700

--- components/CodeBlocks/Code.tsx
+++ components/CodeBlocks/Code.tsx
@@ -1,13 +1,41 @@
 import { classNames } from '@components/utils';
-import type { HTMLProps } from 'react';
+import type { HTMLProps, ReactElement } from 'react';
 import styles from './Code.module.css';
 
 interface Props extends HTMLProps<HTMLPreElement> {}
 
-const Code = ({ children, className, ...props }: Props): JSX.Element => (
-  <pre {...props} className={classNames(className, styles.code)}>
-    {children}
-  </pre>
-);
+const alias = {
+  md: 'Markdown',
+  markdown: 'Markdown',
+  js: 'JavaScript',
+  jsx: 'React',
+  javascript: 'JavaScript',
+  ts: 'TypeScript',
+  tsx: 'React',
+  typescript: 'TypeScript',
+  vue: 'Vue',
+};
+
+type AliasKey = keyof typeof alias;
+
+const getAlias = (language: string) =>
+  alias[language.toLowerCase() as AliasKey] ?? language;
+
+const Code = ({ children, className, ...props }: Props): JSX.Element => {
+  const childrenElement = children as ReactElement;
+  const language = getAlias(
+    (childrenElement?.props?.className as string)?.replace('language-', '') || 'Code'
+  );
+
+  return (
+    <pre
+      {...props}
+      className={classNames(className, styles.code)}
+      data-language={language}
+    >
+      {children}
+    </pre>
+  );
+};
 
 export default Code;
