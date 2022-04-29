18d6d52f48a27fb2074820e14f85684110191b63
Sabertaz <sabertazimi@gmail.com>
2022-04-29 17:59:01 +0800

    refactor(mdx-anchor): move `a` anchor to `Texts` (#724)
    issue #699

--- components/Texts/index.ts
+++ components/Texts/index.ts
@@ -1,3 +1,4 @@
+export { default as Anchor } from './Anchor';
 export { default as Delete } from './Delete';
 export { default as Emphasis } from './Emphasis';
 export { default as Strong } from './Strong';
