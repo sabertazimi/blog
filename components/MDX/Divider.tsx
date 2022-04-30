14d787e63dd9335b08922e2f36088bd287977c3b
Sabertaz <sabertazimi@gmail.com>
2022-05-01 05:44:45 +0800

    fix(mdx-styles): re-design `InlineCode`, `Code` and `Divider` (#751)
    - Remove all border radius.
    - Make `InlineCode` looks lightweight.
    
    issue #700
    issue #701

--- components/MDX/Divider.tsx
+++ components/MDX/Divider.tsx
@@ -1,7 +1,7 @@
 import Divider from '@components/Divider';
 
 const MDXDivider = (): JSX.Element => (
-  <Divider className="mx-0 my-12 h-1 rounded-full bg-gradient-primary" />
+  <Divider className="mx-0 my-12 h-1 bg-gradient-primary" />
 );
 
 export default MDXDivider;
