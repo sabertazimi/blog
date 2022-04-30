ecc32e1ec43035c941b90fd960e411eeead67c7c
Sabertaz <sabertazimi@gmail.com>
2022-05-01 03:38:48 +0800

    fix(components): rectify margin space (#748)
    - Rectify headings space for `ArticleHeader` and `PostCard`.
    - Make list in admonition become more compact.
    
    issue #699

--- components/Article/ArticleHeader.tsx
+++ components/Article/ArticleHeader.tsx
@@ -2,9 +2,9 @@ import { H1 } from '@components/Headings';
 import { Calendar, ClockCircle, Edit } from '@components/Icons';
 import { Slide } from '@components/Motion';
 import { IconTag, LinkTag } from '@components/Tags';
-import { classNames } from '@components/utils';
 import { siteConfig } from '@config';
 import type { PostMeta } from '@types';
+import styles from './ArticleHeader.module.css';
 
 interface Props {
   post: PostMeta;
@@ -13,13 +13,7 @@ interface Props {
 const ArticleHeader = ({
   post: { title, createTime, updateTime, readingTime, tags },
 }: Props): JSX.Element => (
-  <div
-    className={classNames(
-      'w-full p-3 md:px-32 md:pt-48 md:pb-40',
-      'bg-center bg-no-repeat bg-cover',
-      'bg-gradient-primary'
-    )}
-  >
+  <div className={styles.header}>
     <Slide>
       {tags ? (
         tags.map(tag => {
@@ -28,7 +22,7 @@ const ArticleHeader = ({
       ) : (
         <LinkTag href="/tags" color={siteConfig.themeColor} />
       )}
-      <H1 className="my-8 text-light text-6xl md:text-8xl">{title}</H1>
+      <H1 className={styles.title}>{title}</H1>
       <IconTag
         tag={createTime ? new Date(createTime).toDateString() : 'Nowadays'}
         icon={<Calendar />}
