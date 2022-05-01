d87f2be52fab086903f8252f508f9ce99a94577f
sabertazimi <sabertazimi@gmail.com>
2022-05-02 04:46:01 +0800

    refactor(components-className): remove empty `className`

--- components/SocialButton/SocialButton.tsx
+++ components/SocialButton/SocialButton.tsx
@@ -1,28 +1,29 @@
 import Button from '@components/Button';
 import { SocialIcon } from '@components/Icons';
 import { Bounce } from '@components/Motion';
+import { classNames } from '@components/utils';
 import type { SocialType } from '@types';
 import type { HTMLProps } from 'react';
 
 interface Props extends HTMLProps<HTMLButtonElement> {
   type: SocialType;
   url: string;
-  color?: string;
   className?: string;
+  color?: string;
 }
 
 const SocialButton = ({
   type,
   url,
+  className,
   color = '',
-  className = '',
 }: Props): JSX.Element => (
   <Bounce>
     <Button
       role="link"
       size="large"
       type="link"
-      className={`text-center text-light rounded-none ${className}`}
+      className={classNames('text-center text-light rounded-none', className)}
       style={color ? { backgroundColor: color } : {}}
       icon={SocialIcon(type)}
       href={`${url}`}
