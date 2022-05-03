6cb8451fff29efedeb867907614abf8f4799a7a9
sabertazimi <sabertazimi@gmail.com>
2022-05-03 20:22:13 +0800

    fix(CodeBlocks): make highlight lines become selective
    issue #776

--- components/CodeBlocks/LiveCode.test.tsx
+++ components/CodeBlocks/LiveCode.test.tsx
@@ -4,7 +4,7 @@ import LiveCode from './LiveCode';
 describe('LiveCode', () => {
   test('should render live code correctly (snapshot)', () => {
     const { container } = render(
-      <LiveCode className="language-type">Live Code</LiveCode>
+      <LiveCode className="language-type" code="Live Code" />
     );
 
     expect(container).toMatchSnapshot();
