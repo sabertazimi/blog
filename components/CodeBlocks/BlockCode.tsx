e59cfc582050ecd197f64f83fda148fff502ba50
Sabertaz <sabertazimi@gmail.com>
2022-05-03 21:40:00 +0800

    fix(CodeBlocks): normalize code before passing to code blocks (#788)

--- components/CodeBlocks/BlockCode.tsx
+++ components/CodeBlocks/BlockCode.tsx
@@ -3,12 +3,11 @@ import type { Language } from 'prism-react-renderer';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import styles from './BlockCode.module.css';
 import theme from './monokai';
-import { normalizeCode } from './utils';
 
 interface Props {
   enableLine?: boolean;
   lines?: Set<number>;
-  children?: string;
+  children: string;
   className?: string;
 }
 
@@ -17,37 +16,37 @@ const BlockCode = ({
   lines = new Set(),
   children,
   className,
-}: Props): JSX.Element => {
-  const code = normalizeCode(children);
-  const language = className?.replace('language-', '') as Language;
-
-  return (
-    <Highlight {...defaultProps} theme={theme} code={code} language={language}>
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={classNames(className, styles.code)} style={style}>
-          {tokens.map((line, index) => (
-            <div
-              key={index}
-              {...getLineProps({ line, key: index })}
-              className={classNames(
-                'token-line',
-                lines.has(index + 1) ? styles.highlight : styles.hover
-              )}
-            >
-              {enableLine ? (
-                <span className={styles.number}>{index + 1}</span>
-              ) : (
-                <span className={styles.placeholder}></span>
-              )}
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
-  );
-};
+}: Props): JSX.Element => (
+  <Highlight
+    {...defaultProps}
+    theme={theme}
+    code={children}
+    language={className?.replace('language-', '') as Language}
+  >
+    {({ className, style, tokens, getLineProps, getTokenProps }) => (
+      <pre className={classNames(className, styles.code)} style={style}>
+        {tokens.map((line, index) => (
+          <div
+            key={index}
+            {...getLineProps({ line, key: index })}
+            className={classNames(
+              'token-line',
+              lines.has(index + 1) ? styles.highlight : styles.hover
+            )}
+          >
+            {enableLine ? (
+              <span className={styles.number}>{index + 1}</span>
+            ) : (
+              <span className={styles.placeholder}></span>
+            )}
+            {line.map((token, key) => (
+              <span key={key} {...getTokenProps({ token, key })} />
+            ))}
+          </div>
+        ))}
+      </pre>
+    )}
+  </Highlight>
+);
 
 export default BlockCode;
