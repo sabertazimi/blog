95158294103b867e75cac22891d6619507e95d94
sabertazimi <sabertazimi@gmail.com>
2022-05-11 01:33:26 +0800

    ci(lint): add `prettier` format checking

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,10 +1,10 @@
 /**
  * -- This is a parent command --
  * Cypress.Commands.add('login', (email, password) => { ... })
- * 
+ *
  * -- This is a child command --
  * Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
- * 
+ *
  * -- This is a dual command --
  * Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
  *
