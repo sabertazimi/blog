a3911d2784560796cac06bd6d92c867488ad0e69
Sabertaz <sabertazimi@gmail.com>
2022-05-11 01:35:57 +0800

    styles(test): format code (#836)

--- components/Editor/Editor.test.tsx
+++ components/Editor/Editor.test.tsx
@@ -16,9 +16,15 @@ describe('Editor', () => {
     );
 
     expect(screen.getByRole('tablist')).toBeInTheDocument();
-    expect(screen.getByRole('tablist')).toContainElement(screen.getByText('App.tsx'));
-    expect(screen.getByRole('tablist')).toContainElement(screen.getByText('bar.tsx'));
+    expect(screen.getByRole('tablist')).toContainElement(
+      screen.getByText('App.tsx')
+    );
+    expect(screen.getByRole('tablist')).toContainElement(
+      screen.getByText('bar.tsx')
+    );
     expect(screen.getByRole('group')).toBeInTheDocument();
-    expect(screen.getByRole('group')).toContainElement(screen.getByText('const foo = bar();'));
+    expect(screen.getByRole('group')).toContainElement(
+      screen.getByText('const foo = bar();')
+    );
   });
 });
