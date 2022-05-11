290dda9be80d61f90e9eb928cdcf9b3b22b1ae9f
Sabertaz <sabertazimi@gmail.com>
2022-05-11 18:00:34 +0800

    test(e2e): add e2e testing for landing home page (#840)

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,9 +1,43 @@
+import { routes } from '@config';
+
 describe('Home', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
-  it('should display the home page', () => {
+  it('should display home page', () => {
     cy.url().should('include', '/');
+  });
+
+  it('should display home page title', () => {
+    cy.get('span[role="banner"').should('be.visible');
+  });
+
+  it('should toggle navigation menu when navigation button toggled', () => {
+    cy.get('span[data-testid="hamburger-button"]').click();
+    cy.get('div[role="banner"]').should('be.visible');
+    cy.get('div[role="navigation"]')
+      .should('be.visible')
+      .find('span[role="link"]')
+      .should('have.length', routes.length)
+      .should('be.visible');
+
+    cy.get('svg[data-testid="hamburger-icon"]').type('{enter}');
+    cy.get('div[role="banner"]').should('not.be.visible');
+    cy.get('div[role="navigation"]')
+      .should('not.be.visible')
+      .find('span[role="link"]')
+      .should('not.be.visible');
+  });
+
+  it('should contain navigation links to pages', () => {
+    cy.get('span[data-testid="hamburger-button"]').click();
+    cy.get('div[role="navigation"]')
+      .find('span[role="link"]')
+      .each((link, index) => {
+        cy.wrap(link)
+          .contains(routes[index].name)
+          .should('have.attr', 'href', routes[index].path);
+      });
   });
 });
