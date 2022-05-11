d9edb46b5d79f5538e4be0f9d4cf0a09b44566de
Sabertaz <sabertazimi@gmail.com>
2022-05-12 01:05:26 +0800

    fix(cypress-commands): add `testing-library` style query commands (#846)

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,4 +1,16 @@
 /**
  * @see https://on.cypress.io/configuration
  */
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      findByRole(role: string): Chainable<JQuery<HTMLElement>>;
+      findByTestId(testId: string): Chainable<JQuery<HTMLElement>>;
+      getByRole(role: string): Chainable<JQuery<HTMLElement>>;
+      getByTestId(testId: string): Chainable<JQuery<HTMLElement>>;
+    }
+  }
+}
+
 import './commands';
