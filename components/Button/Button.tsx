386b06851fa717bfd2e5bc9a1ab28c7a991e092e
Sabertaz <sabertazimi@gmail.com>
2022-06-15 19:41:22 +0800

    fix(components): remove built-in border radius (#922)

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,8 +1,11 @@
-import { Button as AntButton } from 'antd';
+import { cx } from '@components/utils';
 import type { ButtonProps } from 'antd';
+import { Button as AntButton } from 'antd';
 
 interface Props extends ButtonProps {}
 
-const Button = (props: Props): JSX.Element => <AntButton {...props} />;
+const Button = ({ className, ...props }: Props): JSX.Element => (
+  <AntButton className={cx(className, 'rounded-none')} {...props} />
+);
 
 export default Button;
