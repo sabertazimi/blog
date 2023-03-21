f0d735f585fb1b49ec172a58c3e8de38b447d0a9
Sabertaz <sabertazimi@gmail.com>
2023-03-21 11:07:36 +0800

    fix(gitub): only fetch github data when vercel CI (#1075)

--- lib/getGitHubData.ts
+++ lib/getGitHubData.ts
@@ -2,90 +2,62 @@ import { siteConfig } from '@config';
 import { Octokit } from '@octokit/rest';
 import type { GitHub } from '@types';
 
-let githubData: GitHub = {
-  profile: {
-    username: 'sabertazimi',
-    avatar: 'https://avatars.githubusercontent.com/u/12670482?v=4',
-    bio: 'CS',
-    location: 'Wuhan',
-    url: 'https://github.com/sabertazimi',
-    followers: 42,
-    followersUrl: 'https://github.com/sabertazimi/followers',
-    following: 185,
-    followingUrl: 'https://github.com/sabertazimi/following',
-    createDate: 'Sat May 30 2015',
-  },
-  repos: [
-    {
-      name: 'awesome-notes',
-      stars: 22,
-      language: 'TypeScript',
-      repoUrl: 'https://github.com/sabertazimi/awesome-notes',
-    },
-    {
-      name: 'hust-lab',
-      stars: 21,
-      language: 'C',
-      repoUrl: 'https://github.com/sabertazimi/hust-lab',
-    },
-    {
-      name: 'dragon-zsh-theme',
-      stars: 11,
-      language: 'Shell',
-      repoUrl: 'https://github.com/sabertazimi/dragon-zsh-theme',
-    },
-  ],
-};
-
 export default async function getGitHubData(): Promise<GitHub> {
+  const isVercel = process.env.VERCEL && process.env.NODE_ENV === 'production';
   const octokit = new Octokit();
   const username = siteConfig.socials.github;
+  let githubData = siteConfig.githubData;
 
-  try {
-    const { data: profileJSON } = await octokit.rest.users.getByUsername({
-      username,
-    });
-    const { data: reposJSON } = await octokit.request(
-      'GET /users/{username}/repos',
-      {
+  if (isVercel) {
+    try {
+      const { data: profileJSON } = await octokit.rest.users.getByUsername({
         username,
-      }
-    );
-
-    githubData = {
-      profile: {
-        username: profileJSON.login,
-        avatar: profileJSON.avatar_url,
-        bio: profileJSON.bio,
-        location: profileJSON.location,
-        url: profileJSON.html_url,
-        followers: profileJSON.followers,
-        followersUrl: `${profileJSON.html_url}/followers`,
-        following: profileJSON.following,
-        followingUrl: `${profileJSON.html_url}/following`,
-        createDate: new Date(profileJSON.created_at as string).toDateString(),
-      },
-      repos: reposJSON
-        .filter(repo => (repo.stargazers_count as number) > 0)
-        .sort((repo1, repo2) =>
-          (repo1.stargazers_count as number) <
-          (repo2.stargazers_count as number)
-            ? 1
-            : -1
-        )
-        .map(repo => ({
-          name: repo.name,
-          stars: repo.stargazers_count,
-          language: repo.language,
-          repoUrl: repo.html_url,
-        })),
-    } as GitHub;
-  } catch (error) {
-    if (error instanceof Error) {
-      throw new Error(
-        'GitHub API request error, fallback to local GitHub data.'
+      });
+      const { data: reposJSON } = await octokit.request(
+        'GET /users/{username}/repos',
+        {
+          username,
+        }
       );
+
+      githubData = {
+        profile: {
+          username: profileJSON.login,
+          avatar: profileJSON.avatar_url,
+          bio: profileJSON.bio || '',
+          location: profileJSON.location || '',
+          url: profileJSON.html_url,
+          followers: profileJSON.followers,
+          followersUrl: `${profileJSON.html_url}/followers`,
+          following: profileJSON.following,
+          followingUrl: `${profileJSON.html_url}/following`,
+          createDate: new Date(profileJSON.created_at).toDateString(),
+        },
+        repos: reposJSON
+          .filter(({ stargazers_count = 0 }) => stargazers_count > 0)
+          .sort(
+            (
+              { stargazers_count: stargazers_count1 = 0 },
+              { stargazers_count: stargazers_count2 = 0 }
+            ) => (stargazers_count1 < stargazers_count2 ? 1 : -1)
+          )
+          .map(repo => ({
+            name: repo.name,
+            stars: repo.stargazers_count || 0,
+            language: repo.language || '',
+            repoUrl: repo.html_url,
+          })),
+      };
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+        console.error(
+          'GitHub API request error, fallback to local GitHub data.'
+        );
+      }
     }
+  } else {
+    console.info('Not for Vercel build, fallback to local GitHub data.');
   }
 
   return githubData;
