7e080fa05baa4012f9b892383e7726da5e17746a
sabertazimi <sabertazimi@gmail.com>
2024-04-19 02:13:35 +0800

    refactor(components): move providers to a separate folder

---
+++ components/Providers/index.ts
@@ -0,0 +1,3 @@
+export { default as AntdProvider } from './AntdProvider'
+export { default as ProgressBarProvider } from './ProgressBarProvider'
+export { default as SandpackProvider } from './SandpackProvider'
