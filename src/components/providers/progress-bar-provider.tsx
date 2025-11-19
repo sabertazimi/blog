9e12d500e0f2c06fa5c9879cf1ef4dfcb3dd0101
sabertazimi <sabertazimi@gmail.com>
2025-11-19 21:33:29 +0800

    feat(progress-bar): migrate from nprogress to bprogress

--- src/components/providers/progress-bar-provider.tsx
+++ src/components/providers/progress-bar-provider.tsx
@@ -1,13 +1,12 @@
 'use client'
 
-import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
+import { ProgressProvider } from '@bprogress/next/app'
 import { siteConfig } from '@/config'
 
 export default function ProgressBarProvider({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <ProgressProvider height="4px" color={siteConfig.themeColor} options={{ showSpinner: false }} shallowRouting>
       {children}
-      <ProgressBar height="3px" color={siteConfig.themeColor} />
-    </>
+    </ProgressProvider>
   )
-};
+}
