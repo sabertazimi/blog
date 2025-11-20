24ceeac7442fe9441aaa1931eddcf70043ce9ad3
sabertazimi <sabertazimi@gmail.com>
2025-11-20 15:21:09 +0800

    fix(seo): change language metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="zh-CN">
+    <html lang="en">
       <head>
         <SandpackProvider />
       </head>
