09e2cfddff1c6cb54036bb4cffe7518283c3730a
sabertazimi <sabertazimi@gmail.com>
2025-11-21 23:59:54 +0800

    feat(mdx): implement typography for mdx

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -4,6 +4,7 @@ import type { MDXRemoteSerializeResult } from 'next-mdx-remote'
 import { MDXRemote } from 'next-mdx-remote'
 import dynamic from 'next/dynamic'
 import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 interface MDXProps {
   source: MDXRemoteSerializeResult
@@ -19,7 +20,20 @@ const mdxComponents = {
 }
 
 function MDX({ source }: MDXProps) {
-  return <MDXRemote {...source} components={mdxComponents} />
+  return (
+    <div
+      className={cn(
+        'prose dark:prose-invert prose-lg max-w-none',
+        'prose-headings:font-semibold',
+        'prose-inline-code:before:content-none prose-inline-code:after:content-none',
+        'prose-inline-code:rounded-md prose-inline-code:border prose-inline-code:border-border',
+        'prose-inline-code:bg-muted prose-inline-code:px-1.5 prose-inline-code:py-0.5',
+        'prose-inline-code:font-semibold prose-inline-code:text-foreground prose-inline-code:font-mono',
+      )}
+    >
+      <MDXRemote {...source} components={mdxComponents} />
+    </div>
+  )
 }
 
 export default MDX
